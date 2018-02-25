dc93d9711bbbd3cb15b8cd258c2324e0d5c1d2a2
VimalaKarlapudi <karlapudivimala@gmail.com>
2018-02-25 17:05:26 +0530

    first commit

---
+++ src/app/models/salary.model.ts
@@ -0,0 +1,12 @@
+export class Salary{
+ HRA:number;
+ DA:number;
+ Bonous:string;
+ BasicSal:number;
+    constructor(hra,da,bonous,basic){
+        this.HRA=hra;
+        this.DA=da;
+        this.Bonous=bonous;
+        this.BasicSal=basic;
+    }
+}
